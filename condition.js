7fc18c5c5547e06eab9495baf16f8631ef6cbd8d
Pranjal Animesh <pranjalanimesh5@gmail.com>
2022-11-23 06:59:35 +0000

    initial

---
+++ condition.js
@@ -0,0 +1,13 @@
+const a = false
+const b = true
+if (a){
+
+console.log("its true")
+} else if (b) {
+    console.log("its not true")
+}
+
+else{
+    console.log("its not true")
+    
+}
--- variable.js
+++ variable.js
@@ -1,10 +1,7 @@
-var  x = 69;
-const y = 69;
-let z = 69;
-x = 'something else';
-y = 'cant be changed';
-z = '6969'
 
-console.log(x)
-console.log(y)
-console.log(z)
+const Quote = require('inspirational-quotes')
+
+const = Qu
+let inspire = Quote.getQuote()
+
+console.log(inspire.text)
